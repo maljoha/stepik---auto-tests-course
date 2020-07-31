c9e15c16b1db8051bcaf93ffd14019059603e5c4
Елена <e.lapteva@etpgpb.ru>
2020-08-01 01:27:59 +0300

    add a homework file

---
+++ lesson2.4_step8.py
@@ -0,0 +1,50 @@
+'''
+Попробуем теперь написать программу, которая будет бронировать нам дом для отдыха по строго заданной цене. Более высокая цена нас не устраивает, а по более низкой цене объект успеет забронировать кто-то другой.
+
+В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий:
+
+Открыть страницу http://suninjuly.github.io/explicit_wait2.html
+Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)
+Нажать на кнопку "Book"
+Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение
+Чтобы определить момент, когда цена аренды уменьшится до $100, используйте метод text_to_be_present_in_element из библиотеки expected_conditions.
+
+Если все сделано правильно и быстро, то вы увидите окно с числом. Отправьте его в качестве ответа на это задание.
+'''
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    # Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)й
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+
+    # Нажать на кнопку "Book"
+    button = browser.find_element_by_id("book")
+    button.click()
+
+    # Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    y_element = browser.find_element_by_id("answer")
+    y_element.send_keys(str(y))
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+finally:
+    # успеваем посмотреть результаты за 5 секунд
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
